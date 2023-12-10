32a20d9bf9821671f41e86b6a2bfe267703c375e
SergeyBirukov <birukovsergej121@gmail.com>
2023-12-10 15:05:34 +0500

    Add config to rest app

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,11 +1,14 @@
+import { ConfigInterface } from '../core/config/config.interface';
 import { LoggerInterface } from '../logger/logger.interface';
 
 export default class RestApplication {
     constructor(
-      private readonly logger: LoggerInterface
+      private readonly logger: LoggerInterface,
+      private readonly config: ConfigInterface
     ) {}
   
     public async init() {
       this.logger.info('Application initialization…');
+      this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
     }
   }
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import PinoService from './services/pino.service.js';
+import RestApplication from './app/rest.js';
+import ConfigService from './core/config/config.service.js';
+
+async function bootstrap() {
+  const logger = new PinoService();
+  const config = new ConfigService(logger);
+
+  const application = new RestApplication(logger, config);
+  await application.init();
+}
